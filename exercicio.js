86427969ea87180d6245f5d1aa5db71d6f647335
Mauricio Sousa <mauriciosouza8989@gmail.com>
2023-12-21 18:26:35 -0300

    Entrega do exercício

---
+++ exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicacao(n1, n2) {
+    return n1 * n2;
+}
+;
+console.log(`O resultado é: ${multiplicacao(2, 13)}`);
+function saldacao(nome) {
+    return 'Olá ' + nome + '!';
+}
+;
+console.log(saldacao('Maurício Sousa'));
---
+++ exercicio.ts
@@ -0,0 +1,9 @@
+function multiplicacao(n1: number, n2: number){
+    return n1 * n2;
+};
+console.log(`O resultado é: ${multiplicacao(2, 13)}`);
+
+function saldacao(nome: string){
+    return 'Olá '+ nome + '!';
+};
+console.log(saldacao('Maurício Sousa'));
